c6a84a97241d63b86c1a240360dc4ceda4d54587
briceallard <briceallard@gmail.com>
2020-09-11 22:20:02 -0500

    added sportsreference nfl api

--- Menu.py
+++ Menu.py
@@ -1,22 +1,31 @@
 import Utils
 
+from sportsreference.nfl.teams import Teams
+
 
 def menu():
+    closeApp = False
     Utils.clear()
 
-    print('SportsBot - Main Menu\n')
-    print('Search options:')
-    print('1. Year')
-    print('2. Player')
-    print('3. Team')
-    print('4. Position')
+    while closeApp == False:
 
-    selection = input("\nMenu Choice: ")
+        print('SportsBot - Main Menu\n')
+        print('Search options:')
+        print('1. Year')
+        print('2. Player')
+        print('3. Team')
+        print('4. Position')
+        print('5. Exit')
 
-    if isMenuChoice(selection, 1, 4):
-        print('Selection found')
+        selection = input("\nMenu Choice: ")
 
-    print('You selected', selection)
+        if selection == '1':
+            year = input('Enter Search Year: ')
+
+            teams = Teams(year)
+
+            for team in teams:
+                print(team.name, team.abbreviation)
 
 
 def isMenuChoice(selection, min, max):
